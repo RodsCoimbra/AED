da0f6e24621d1bf7ba6d795f8feb8707845cafbc
Rodrigo Coimbra <rodscoimbra@gmail.com>
2021-10-26 18:27:19 +0100

    Update qsortwords.c

--- Lab/Lab5/qsortwords.c
+++ Lab/Lab5/qsortwords.c
@@ -72,47 +72,47 @@ void sort(Item a[], int l, int r, int (*less)(Item, Item))
     for (;;)
     {
         OP_CNT++;
-        while (less(a[++i], v))
+        while (less(a[++i], v)) //Começa no inicio e procura até encontrar um número maior ou igual a v
         {
             OP_CNT++;
         }
         OP_CNT++;
-        while (less(v, a[--j]))
+        while (less(v, a[--j])) //Começa no final -1 e procura até encontrar um número menor ou igual a v
         {
             OP_CNT++;
-            if (j == l)
+            if (j == l) //Caso em que não encontra nenhum menor ou igual a v
                 break;
         }
-        if (i >= j)
+        if (i >= j) //Se já tiver o maior à direita e o menor à esquerda sai do for(;;)
             break;
         exch((a[i]), (a[j]));
         OP_CNT += 4;
-        if (eq(a[i], v))
+        if (eq(a[i], v))    
         {
             p++;
-            exch((a[p]), (a[i]));
+            exch((a[p]), (a[i])); //Mete o a[i] no canto esquerdo
             OP_CNT += 2;
         }
         OP_CNT += 2;
         if (eq(a[j], v))
         {
             q--;
-            exch((a[q]), (a[j]));
+            exch((a[q]), (a[j])); //Mete o a[j] no canto direito
             OP_CNT += 2;
         }
     }
-    exch((a[i]), (a[r]));
+    exch((a[i]), (a[r])); //Meter o pivot no sitio certo da lista, onde tudo à esquerda é menor e tudo à direita é maior
     OP_CNT += 2;
     j = i - 1;
     i = i + 1;
-    for (k = l; k <= p; k++, j--)
+    for (k = l; k <= p; k++, j--) //traz os do canto esquerdo para o lado esquerdo do pivot, visto que era o mesmo número a posição deles tem de ser ali
         exch((a[k]), (a[j]));
     OP_CNT += 2;
-    for (k = r - 1; k >= q; k--, i++)
+    for (k = r - 1; k >= q; k--, i++) //traz os do canto direito para o lado direito do pivot, visto que era o mesmo número a posição deles tem de ser ali
         exch((a[k]), (a[i]));
     OP_CNT += 2;
-    sort(a, l, j, less);
-    sort(a, i, r, less);
+    sort(a, l, j, less);    //volta a chamar as funções do inicio até ao número anterior metido por ultimo na esquerda naquela troca de cima 
+    sort(a, i, r, less);    //volta a chamar as funções desde do número a seguir ao metido por ultimo na direita naquela troca de cima, até ao fim
 }
 
 /******************************************************************************

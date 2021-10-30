e30ded048c37aa03c3a50e4a485acbb8c0d00e47
Rodrigo Coimbra <rodscoimbra@gmail.com>
2021-10-30 16:31:51 +0100

    Primeira parte do lab completa

--- Lab/Lab6/pg1.c
+++ Lab/Lab6/pg1.c
@@ -14,37 +14,32 @@ int main(int argc, char *argv[])
     FILE *fp, *fp2;
     char *ent, *saida;
     G *g;
-    int i, j;
+    int i, j, grau = 0, aux;
+    double den = 0;
     if (argc < 2)
     {
-        printf("Entrei1");
         exit(-1);
     }
     ent = argv[1];
     saida = (char *)malloc((strlen(argv[1]) + 1) * sizeof(char));
     if (ent == NULL || saida == NULL)
     {
-        printf("Entrei2");
         exit(-1);
     }
     strncpy(saida, ent, strlen(argv[1]) - 3);
     strcat(saida, "edge");
-    printf("%s", ent);
     if ((fp = fopen(ent, "r")) == NULL)
     {
-        printf("Entrei3");
         exit(-1);
     }
     if ((fp2 = fopen(saida, "w")) == NULL)
     {
 
-        printf("Entrei4");
         exit(-1);
     }
     g = (G *)calloc(1, sizeof(G));
     if (g == NULL)
     {
-        printf("Entrei5");
         exit(-1);
     }
     fscanf(fp, "%d", &g->V);
@@ -58,15 +53,43 @@ int main(int argc, char *argv[])
         for (j = 0; j < (g->V); j++)
         {
             fscanf(fp, "%d", &(g->table[i][j]));
+            if (g->table[i][j] != 0)
+            {
+                g->E++;
+            }
         }
     }
+
     for (i = 0; i < (g->V); i++)
     {
-        for (j = 0; j < (g->V); j++)
+        for (j = i + 1, aux = 0; j < (g->V); j++)
         {
-            printf("%d", g->table[i][j]);
+            if (g->table[i][j] != 0)
+            {
+                aux++;
+            }
+        }
+        if (grau < aux)
+        {
+            grau = aux;
         }
     }
+    g->E /= 2;
+    /*Densidade*/
+    den = (double)(2 * g->E) / (g->V);
+    printf("Densidade %.3f\t grau máximo %d\n", den, grau);
+    fprintf(fp2, "Vertice:%d\tAresta:%d", g->V, g->E);
+    for (i = 0; i < (g->V); i++)
+    {
+        for (j = i + 1, aux = 0; j < (g->V); j++)
+        {
+            if (g->table[i][j] != 0)
+            {
+                fprintf(fp2, "\n%d - %d Custo:%d", i, j, g->table[i][j]);
+            }
+        }
+    }
+
     fclose(fp);
     fclose(fp2);
     free(saida);
